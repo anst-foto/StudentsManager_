1e7655635191629b48c3939d8065e43fbf25ace5
Starinin Andrey <starinin-andrey@ya.ru>
2025-09-01 19:40:18 +0300

    Доработка

--- StudentsManager.Desktop/Model/Account.cs
+++ StudentsManager.Desktop/Model/Account.cs
@@ -1,18 +1,29 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace StudentsManager.Desktop.Model;
 
 public class Account
 {
+    [JsonPropertyName("id")]
     public required Guid Id { get; init; }
     
+    [JsonPropertyName("last_name")]
     public required string LastName { get; init; }
+    
+    [JsonPropertyName("first_name")]
     public required string FirstName { get; init; }
     
+    [JsonIgnore]
     public string FullName => $"{LastName} {FirstName}";
     
+    [JsonPropertyName("login")]
     public required string Login { get; init; }
+    
+    [JsonPropertyName("password")]
     public required string Password { get; init; }
 
     public static IEnumerable<Account>? Load(string path = "accounts.json")
--- StudentsManager.Desktop/Windows/AuthWindow.xaml.cs
+++ StudentsManager.Desktop/Windows/AuthWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using StudentsManager.Desktop.Model;
 
 namespace StudentsManager.Desktop.Windows;
